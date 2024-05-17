a9aef043bf8249a617a8053c72d08c79ca5aa7d6
blocklinklabs <blocklinklabs@gmail.com>
2024-05-17 06:54:01 +0000

    update README.md, hero-section.tsx and next.config.js

--- app/(landing-page)/hero-section.tsx
+++ app/(landing-page)/hero-section.tsx
@@ -9,9 +9,7 @@ import { motion } from "framer-motion";
 import { useRef } from "react";
 import { useInView } from "framer-motion";
 
-
 import { useMediaQuery } from "react-responsive";
-
 
 import { useState } from "react";
 import {
@@ -21,7 +19,6 @@ import {
   PiSparkleLight,
   PiTargetLight,
 } from "react-icons/pi";
-
 
 const tabs = [
   {
@@ -85,34 +82,21 @@ const tabs = [
 const HeroSection = () => {
   const ref = useRef(null);
 
-
   const [activeTab, setActiveTab] = useState(tabs[0]);
 
   const isSmallScreen = useMediaQuery({ maxWidth: 767 });
- 
-
-
-
-
 
   return (
-
-      <div className="md:items-center flex flex-col ">
-        <div
-          className="
-     
+    <div className="md:items-center flex flex-col ">
+      <div
+        className="
           font-medium
-        
           2xl:w-1/3
           md:w-2/3
           xl:w-1/2
-          
-          
           lg:px-0
           px-8
-    
-
-  text-5xl
+          text-5xl
             xl:text-6xl     
             flex
             justify-center
@@ -121,51 +105,52 @@ const HeroSection = () => {
             text-center 
             pt-6
             "
-        >
-          Write, plan, share. With AI at your side.
-        </div>
+      >
+        Your Digital Identity, Reinvented{" "}
+      </div>
 
-        <p
-          className="
+      <p
+        className="
             text-2xl
             pt-4
             text-center
             w-2/3
             mx-auto
             "
-        >
-          Bird is the connected workspace where better, faster work happens.
-        </p>
+      >
+        Create Once, Identify Everywhere with
+        <span className="text-sky-500">IdentiFi</span>
+      </p>
 
-        <div className="flex gap-4 pt-6 items-center justify-center">
-          <Link href="/">
-            <Button className="py-1 ">
-              <div className="flex items-center justify-center">
-                <div className="text-lg">Get bird free</div>
-                <div>
-                  <PiArrowRight className="ml-2 " />
-                </div>
+      <div className="flex gap-4 pt-6 items-center justify-center">
+        <Link href="/">
+          <Button className="py-1 ">
+            <div className="flex items-center justify-center">
+              <div className="text-lg">Create Identity</div>
+              <div>
+                <PiArrowRight className="ml-2 " />
               </div>
-            </Button>
-          </Link>
-        </div>
+            </div>
+          </Button>
+        </Link>
+      </div>
 
-        <div className="pt-10 xl:pt-20 items-center justify-center">
-          <Image
-            src="/assets/ReadingSideDoodle.svg"
-            alt="hero image"
-            width={1000}
-            height={1000}
-            className="flex items-center justify-center mx-auto w-60 xl:w-80"
-          />
-        </div>
-        {isSmallScreen ? (
-          <div className="px-8">
-         <div className="grid grid-cols-4   md:row-span-1  gap-4  xl:gap-6 mt-8   xl:px-0  ">
-        {tabs.map((tab) => (
-          <motion.div
-            key={tab.name}
-            className={`
+      <div className="pt-10 xl:pt-20 items-center justify-center">
+        <Image
+          src="/assets/ReadingSideDoodle.svg"
+          alt="hero image"
+          width={1000}
+          height={1000}
+          className="flex items-center justify-center mx-auto w-60 xl:w-80"
+        />
+      </div>
+      {isSmallScreen ? (
+        <div className="px-8">
+          <div className="grid grid-cols-4   md:row-span-1  gap-4  xl:gap-6 mt-8   xl:px-0  ">
+            {tabs.map((tab) => (
+              <motion.div
+                key={tab.name}
+                className={`
               flex 
             p-1
               md:p-8
@@ -175,33 +160,31 @@ const HeroSection = () => {
         
                 ${
                   activeTab.name === tab.name
-                  ? "rounded-md md:rounded-xl bg-[#f6f5f4]  md:bg-white border-gray-200 md:border items-center justify-center flex p-1 "
-                  : "md:bg-[#f6f5f4]   rounded-md xl:rounded-xl p-1 items-center justify-center hover:bg-[#eae7e7] "
-                } `
-            }
-                
-            onClick={() => setActiveTab(tab)}
-          >
-            <div className="flex flex-col   items-center md:justify-center mx-auto">
-              <div className="hidden md:flex text-4xl">{tab.icon}</div>
-              <div className="font-medium text-sm  xl:text-lg mt-1">
-                {tab.name}
-              </div>
-            </div>
-          </motion.div>
-        ))}
-      </div>
+                    ? "rounded-md md:rounded-xl bg-[#f6f5f4]  md:bg-white border-gray-200 md:border items-center justify-center flex p-1 "
+                    : "md:bg-[#f6f5f4]   rounded-md xl:rounded-xl p-1 items-center justify-center hover:bg-[#eae7e7] "
+                } `}
+                onClick={() => setActiveTab(tab)}
+              >
+                <div className="flex flex-col   items-center md:justify-center mx-auto">
+                  <div className="hidden md:flex text-4xl">{tab.icon}</div>
+                  <div className="font-medium text-sm  xl:text-lg mt-1">
+                    {tab.name}
+                  </div>
+                </div>
+              </motion.div>
+            ))}
+          </div>
 
-      {/* Display content based on the active tab */}
-      <div className="pt-6 md:py-10   lg:px-16 xl:px-0 md:px-16  w-full ">
-        {activeTab && (
-          <div className=" flex justify-center items-center flex-col  ">
-            <Image
-              src={activeTab.image}
-              width={1025}
-              height={500}
-              alt="logo"
-              className="
+          {/* Display content based on the active tab */}
+          <div className="pt-6 md:py-10   lg:px-16 xl:px-0 md:px-16  w-full ">
+            {activeTab && (
+              <div className=" flex justify-center items-center flex-col  ">
+                <Image
+                  src={activeTab.image}
+                  width={1025}
+                  height={500}
+                  alt="logo"
+                  className="
                 w-full
              border
              p-20
@@ -210,21 +193,17 @@ const HeroSection = () => {
           
              
                 "
-            />
+                />
+              </div>
+            )}
           </div>
-        )}
-      </div>
-          </div>
-
-  
-           
-
-        ) : (
-          <div className="flex  xl:space-x-4 items-center justify-between hover:cursor-pointer gap-4 w-4/5 xl:w-3/4 2xl:w-[55%]">
-            {tabs.map((tab) => (
-              <motion.div
-                key={tab.name}
-                className={`
+        </div>
+      ) : (
+        <div className="flex  xl:space-x-4 items-center justify-between hover:cursor-pointer gap-4 w-4/5 xl:w-3/4 2xl:w-[55%]">
+          {tabs.map((tab) => (
+            <motion.div
+              key={tab.name}
+              className={`
                 xl:flex 
                 justify-center 
                 space-x-4
@@ -240,61 +219,59 @@ const HeroSection = () => {
                     : "shadow-md rounded-xl pt-2  bg-[#f6f5f4] m"
                 }
               `}
-                onMouseEnter={() => setActiveTab(tab)}
-              >
-                <div className="px-4">
-                  <div className="flex items-center">
-                    <div>{tab.icon}</div>
-                    <div className="text-2xl font-medium">{tab.name}</div>
+              onMouseEnter={() => setActiveTab(tab)}
+            >
+              <div className="px-4">
+                <div className="flex items-center">
+                  <div>{tab.icon}</div>
+                  <div className="text-2xl font-medium">{tab.name}</div>
 
-                    {/* Render feature tag only for "AI" tab */}
-                    {tab.name === "AI" && (
-                      <div className="text-xs font-medium text-purple-600 bg-purple-100 px-2 py-1 rounded-full ml-2">
-                        {tab.feature}
-                      </div>
-                    )}
+                  {/* Render feature tag only for "AI" tab */}
+                  {tab.name === "AI" && (
+                    <div className="text-xs font-medium text-purple-600 bg-purple-100 px-2 py-1 rounded-full ml-2">
+                      {tab.feature}
+                    </div>
+                  )}
+                </div>
+
+                <motion.div
+                  className="flex flex-col text-sm"
+                  initial={{ y: 0 }}
+                  animate={{ y: activeTab === tab ? 10 : 25 }}
+                  transition={{ duration: 0.2 }}
+                >
+                  <div>
+                    {/* Only animate the description */}
+                    <motion.div
+                      initial={{ opacity: 0, y: 10 }}
+                      animate={{ opacity: 1, y: 0 }}
+                      transition={{ duration: 0.3 }}
+                    >
+                      {tab.description}
+                    </motion.div>
                   </div>
 
-                  <motion.div
-                    className="flex flex-col text-sm"
-                    initial={{ y: 0 }}
-                    animate={{ y: activeTab === tab ? 10 : 25 }}
-                    transition={{ duration: 0.2 }}
-                  >
-                    <div>
-                      {/* Only animate the description */}
-                      <motion.div
-                        initial={{ opacity: 0, y: 10 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 0.3 }}
-                      >
-                        {tab.description}
-                      </motion.div>
-                    </div>
+                  {/* Conditional rendering for "Learn more" link */}
+                  {activeTab === tab && (
+                    <div className="text-sm mt-2">{tab.more}</div>
+                  )}
+                </motion.div>
+              </div>
+            </motion.div>
+          ))}
+        </div>
+      )}
 
-                    {/* Conditional rendering for "Learn more" link */}
-                    {activeTab === tab && (
-                      <div className="text-sm mt-2">{tab.more}</div>
-                    )}
-                  </motion.div>
-                </div>
-              </motion.div>
-            ))}
-          </div>
-
-     
-        )}
-
-        {/* Display content based on the active tab */}
-        <div className="hidden md:flex py-10 px-8 md:px-0    lg:w-3/4 2xl:w-[55%]">
-          {activeTab && (
-            <div className=" md:flex  items-center justify-center space-x-6 hover:cursor-pointer w-full">
-              <Image
-                src={activeTab.image}
-                width={500}
-                height={500}
-                alt="logo"
-                className="
+      {/* Display content based on the active tab */}
+      <div className="hidden md:flex py-10 px-8 md:px-0    lg:w-3/4 2xl:w-[55%]">
+        {activeTab && (
+          <div className=" md:flex  items-center justify-center space-x-6 hover:cursor-pointer w-full">
+            <Image
+              src={activeTab.image}
+              width={500}
+              height={500}
+              alt="logo"
+              className="
                 w-full
                 p-20
                 xl:p-40
@@ -302,12 +279,11 @@ const HeroSection = () => {
                 rounded-xl
                 bg-[#f6f5f4]
         "
-              />
-            </div>
-          )}
-        </div>
+            />
+          </div>
+        )}
       </div>
-
+    </div>
   );
 };
 
