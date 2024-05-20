395d1b1d500dd5b2dc067556666a343aa7e92411
blocklinklabs <blocklinklabs@gmail.com>
2024-05-20 13:25:51 +0000

    update solidity file

--- ignition/modules/Lock.js
+++ ignition/modules/Lock.js
@@ -1,5 +1,5 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
+// exporiting smart contract address
 module.exports = buildModule("IdentiFi", (m) => {
   const identiFi = m.contract("IdentiFi");
 
