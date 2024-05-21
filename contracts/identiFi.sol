7da6916135131d285601f097e48c28d283b2311e
blocklinklabs <blocklinklabs@gmail.com>
2024-05-21 17:17:30 +0000

    update identiFi.sol

--- contracts/identiFi.sol
+++ contracts/identiFi.sol
@@ -126,7 +126,7 @@ contract IdentiFi is VRFConsumerBase {
         user.visibility = visibility;
         usernames[username] = true;
         addressToUsername[msg.sender] = username;
-        requestNewDID(); // Request a new DID for the user
+        requestNewDID(); // Request a new DID for the users
     }
 
     function editUser(
