ad77ed8ad5c42cc4ffd258fc222c5098b72e82a5
blocklinklabs <blocklinklabs@gmail.com>
2024-05-25 21:13:34 +0000

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,13 +41,11 @@ export default function RootLayout({
         <PrivyProvider
           appId="clwci9fzm00w7ro85bx6jarr5"
           config={{
-            // Customize Privy's appearance in your app
             appearance: {
               theme: "light",
               accentColor: "#676FFF",
               logo: "https://your-logo-url",
             },
-            // Create embedded wallets for users who don't have a wallet
             embeddedWallets: {
               createOnLogin: "users-without-wallets",
             },
