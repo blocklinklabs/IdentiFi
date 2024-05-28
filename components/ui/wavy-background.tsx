345510700a40e03917953a356f071184234afaaf
blocklinklabs <blocklinklabs@gmail.com>
2024-05-28 14:57:43 +0000

    update wavy-background.tsx

--- components/ui/wavy-background.tsx
+++ components/ui/wavy-background.tsx
@@ -76,7 +76,7 @@ export const WavyBackground = ({
       ctx.strokeStyle = waveColors[i % waveColors.length];
       for (x = 0; x < w; x += 5) {
         var y = noise(x / 800, 0.3 * i, nt) * 100;
-        ctx.lineTo(x, y + h * 0.5); // adjust for height, currently at 50% of the container
+        ctx.lineTo(x, y + h * 0.5);
       }
       ctx.stroke();
       ctx.closePath();
